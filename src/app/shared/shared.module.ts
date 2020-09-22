3a5ef102f295c723e781fce61b7a2f3f91c89e77
asotog <alejandro.soto@rivetlogic.com>
2020-09-22 10:00:37 -0600

    added bootstrap to style poc

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PostsService } from './posts.service';
+import { NavigationComponent } from './navigation/navigation.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [NavigationComponent],
+  exports: [NavigationComponent],
+  imports: [CommonModule, RouterModule],
   providers: [
     {
       provide: PostsService,
