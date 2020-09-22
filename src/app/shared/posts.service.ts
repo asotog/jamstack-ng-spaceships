6403376e25d7762f69d6bf8021107b55b9050e3c
asotog <alejandro.soto@rivetlogic.com>
2020-09-21 23:29:48 -0600

    added graphql code generation

--- src/app/shared/posts.service.ts
+++ src/app/shared/posts.service.ts
@@ -1,62 +1,54 @@
 import { Injectable } from '@angular/core';
+import { ApolloQueryResult } from '@apollo/client/core';
 import { isScullyGenerated, TransferStateService } from '@scullyio/ng-lib';
-import { Apollo, gql } from 'apollo-angular';
 import { tap } from 'rxjs/operators';
+import {
+  BlogSamplesGQL,
+  BlogSamplesQuery,
+  BlogSampleGQL,
+  BlogSampleQuery,
+} from './graphql.gen';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PostsService {
   constructor(
-    private apollo: Apollo,
+    private blogSamplesGQL: BlogSamplesGQL,
+    private blogSampleGQL: BlogSampleGQL,
     private transferStateService: TransferStateService
   ) {}
 
   public getPosts() {
     if (isScullyGenerated()) {
-      return this.transferStateService.getState<any>(`/posts`);
+      return this.transferStateService.getState<
+        ApolloQueryResult<BlogSamplesQuery>
+      >(`/posts`);
     }
-    return this.apollo
-      .watchQuery<any>({
-        query: gql`
-          query BlogSamplesQuery {
-            blogSamples {
-              id
-              title
-            }
-          }
-        `,
-      })
+    return this.blogSamplesGQL
+      .watch()
       .valueChanges.pipe(
-        tap((data) => this.transferStateService.setState<any>(`/posts`, data))
+        tap((data) =>
+          this.transferStateService.setState<
+            ApolloQueryResult<BlogSamplesQuery>
+          >(`/posts`, data)
+        )
       );
   }
 
   public getPost(postId: string) {
     if (isScullyGenerated()) {
-      return this.transferStateService.getState<any>(`/p/${postId}`);
+      return this.transferStateService.getState<
+        ApolloQueryResult<BlogSampleQuery>
+      >(`/p/${postId}`);
     }
-    return this.apollo
-      .watchQuery<any>({
-        query: gql`
-        query BlogSampleQuery {
-          blogSample(where: { id: "${postId}" }) {
-            id
-            title
-            content {
-              html
-            }
-            image {
-              url
-            }
-            credits
-          }
-        }
-      `,
-      })
+    return this.blogSampleGQL
+      .watch({ id: postId })
       .valueChanges.pipe(
         tap((data) =>
-          this.transferStateService.setState<any>(`/p/${postId}`, data)
+          this.transferStateService.setState<
+            ApolloQueryResult<BlogSampleQuery>
+          >(`/p/${postId}`, data)
         )
       );
   }
