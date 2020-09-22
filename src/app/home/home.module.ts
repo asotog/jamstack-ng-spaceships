9da8caedd30861944b123ecb456d8caecdd6380f
asotog <alejandro.soto@rivetlogic.com>
2020-09-22 10:31:16 -0600

    added about page

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,13 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  declarations: [HomeComponent, AboutComponent],
+  imports: [CommonModule, HomeRoutingModule],
 })
-export class HomeModule { }
+export class HomeModule {}
